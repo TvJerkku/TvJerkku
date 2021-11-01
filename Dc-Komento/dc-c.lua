685a1e511eedc604ea58e42f5c898fdd1b28732b
TvJerkku <83401997+TvJerkku@users.noreply.github.com>
2021-11-01 13:16:10 +0200

    Delete Dc-Komento directory

--- Dc-Komento/dc-c.lua
+++
@@ -1,7 +0,0 @@
-Registercommand("dc", function()
-    msg("Discord: discord.me/exciterp")
-end, false)
-
-function msg(text)
-    TriggerEvent("chatmessage", "[Discordin paljastaja]", {0,0,255}, text)
-end    
